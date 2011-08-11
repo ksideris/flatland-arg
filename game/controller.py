168dafec80206826d087aa266285286340ea4806
David Elliott <david@hadto.net>
2011-08-11 16:33:23 -0700

    completed implementing nonblocking gesture recognition, need to test and pass values in place of key presses

--- game/controller.py
+++ game/controller.py
@@ -53,10 +53,15 @@ class PlayerController(object):
         self._attackLeft = loadPattern("pickles/attackLeftPattern.pickle")
         self._scan = loadPattern("pickles/scanPattern.pickle")
         self._build = loadPattern("pickles/buildPattern.pickle")
-        self._bestFit = {}
-        self._sample = None
-        self._currentSerialData = None
-        self._scepterBtnPressed = False
+        self._areas = loadPattern("pickles/areas.pickle")
+        #this is a list of area transistions
+        self._sampleData = None
+        self._transitionAverages = None
+        #this is the current serial data
+        self._serialData = None
+        self._currentPattern = None
+        self._sampleCnt = 0
+        self._lastPosition = (0,0,0)
         try:
             self._ser = serial.Serial('/dev/ttyUSB0', 9600)
             self._ser.readline()
@@ -125,17 +130,18 @@ class PlayerController(object):
         time = pygame.time.get_ticks()
         self._updatePosition((time - self.previousTime) / 1000.0)
         self.previousTime = time
-        #If player is pressing red button on scepter take two samples and add them to the average
+        #If player is pressing red button on scepter take two samples, add them to the average, match to predefined patterns
         self._serialData = readSerial()
         if (self._serialData[3]):
-            # TODO: To make this non blocking we would want to do this in two stages
-            # essentially rewrite getSampleData as two seperate functions:
-            # buildSampleData() and averageSampleData()
-            self._sample = getSampleData(2, self._sample)
-            currentPattern = matchPattern()
+            buildSampleData()
+            if(self._sampleCnt == 2): 
+                averageSampleData()
+                self._currentPattern = matchPattern()
         else:
-            currentPattern = None
-            
+            #no action button pressed, no pattern matched, averages erased
+            self._currentPattern = None
+            #this could cause problems with unreliable serial connection, need to test
+            self._transitionAverages = initPattern(1)
 
         for event in pygame.event.get():
             if (event.type == pygame.QUIT) or ((event.type == pygame.KEYDOWN) and (event.key == QUIT)):
@@ -153,13 +159,14 @@ class PlayerController(object):
             self._startedAction(self._actionQueue.pop())
 
     def matchPattern(self):
-        bestFit['attackRight'] = patternDifference(self._sample, attackRight)
-        bestFit['attackLeft'] = patternDifference(self._sample, attackLeft)
-        bestFit['build'] = patternDifference(self._sample, build)
-        bestFit['scan'] = patternDifference(self._sample, scan)
+        bestFit = {}
+        bestFit['attackRight'] = patternDifference(self._transitionAverages, attackRight)
+        bestFit['attackLeft'] = patternDifference(self._transitionAverages, attackLeft)
+        bestFit['build'] = patternDifference(self._transitionAverages, build)
+        bestFit['scan'] = patternDifference(self._transitionAverages, scan)
         return min(bestFit, key=bestFit.get)
 
-    def patternDifference(a,b):
+    def patternDifference(self,a,b):
         totalDifference = float(sys.maxint)
         for i in a.keys():
             for j in a[i].keys():
@@ -167,49 +174,45 @@ class PlayerController(object):
         totalDifference = sys.maxint - totalDifference
         return totalDifference
 
-
-    def getSampleData(sampleLength, averageSoFar=None):
+    def buildSampleData(self):
         """
-        Get some sample data in the form of a nested dictionary.m length
-        Value is the percentage of times a transition happened, transition is defined in the dictionary
-        Keys of dictionaries are tuples that represent spacial coordinates.
+        reads the accelerometer and finds what area it's in. 
+        if the area is different from the last area checked, record the transition in self._tempData
         """
-        sampleData = initPattern(1)
-        lastPosition = (0,0,0)
-        areas = loadPattern("pickles/areas.pickle")
-        counter = 0
-        #here we would get rid of the while loop and just do this once per call in buildSampleData()
-        while counter < sampleLength:
-            keys = ['x', 'y', 'z']
-            data = {'x': self._serialData[0], 'y': self._serialData[1], 'z': self._serialData[2]}
-            results = {}
-            for k in keys:
-                if data[k] < areas[k][0]: results[k] = 0
-                elif data[k] < areas[k][1]: results[k] = 1
-                else: results[k] = 2
-
-            currentPosition = (results['x'], results['y'], results['z'])
-            if lastPosition != currentPosition:
-                sampleData[lastPosition][currentPosition]+=1
-                lastPosition = currentPosition
-                #this counter could be a global property that is used to decide when sample data is built and ready to be averaged
-                counter+=1
-
-        #here is where we would break off the function to build averageSampleData()
-        temp = deepcopy(sampleData)
+        keys = ['x', 'y', 'z']
+        data = readSerial()
+        data = {'x': data[0], 'y': data[1], 'z': data[2]}
+        results = {}
+        for k in keys:
+            if data[k] < areas[k][0]: results[k] = 0
+            elif data[k] < areas[k][1]: results[k] = 1
+            else results[k] = 2
+        currentPosition = (results['x'], results['y'], results['z'])
+        if lastPosition != currentPosition:
+            self._sampleData[lastPosition][currentPosition] += 1
+            self._lastPosition = currentPosition
+            self._sampleCnt += 1            
+        
+    def averageSampleData(self):
+        """
+        when two new transistions have been recored by buildSampleData()
+        this function takes self._tempData and averages it with self._sampleData
+        """
+        temp = deepcopy(self._sampleData)
         for i in temp.keys():
             for j in temp[i].keys():
-                sampleData[i][j] = temp[i][j] / float(sampleLength)
-        if averageSoFar:
-            temp = deepcopy(sampleData)
+                self._sampleData[i][j] = temp[i][j] / float(self._sampleCnt)
+        if (self._transitionAverages):
+            temp = deepcopy(self._transitionAverages)
             for i in temp.keys():
                 for j in temp[i]:
-                    sampleData[i][j] = (temp[i][j] + averageSoFar[i][j]) / 2.0
-        return sampleData
+                    self._transitionAverages[i][j] = ( temp[i][j] + self._sampleData[i][j] ) / 2.0
+        #reset vars for buildSampleData()
+        self._sampleData = initPattern(1)
+        self._sampleCnt = 0
 
 
-
-    def readSerial():
+    def readSerial(self):
         try:
             data = ser.readline()
         except (KeyboardInterrupt, SystemExit):
@@ -220,3 +223,16 @@ class PlayerController(object):
             data = data.split()
             for i in range(len(data)): data[i] = int(data[i])
             return data
+
+
+    def initPattern(self,level):
+        p = {}
+        for i in range(3):
+            for j in range(3):
+                for k in range(3):
+                    if level > 0:
+                        p[i,j,k] = initPattern(level-1)
+                    else:
+                        p[i,j,k] = 0
+                    
+        return p
